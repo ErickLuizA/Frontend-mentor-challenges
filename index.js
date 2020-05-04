f47d437d45d81c83a8fa8f960c4b14154d074d47
erickLuizA <erickluiz69@gmail.com>
2020-05-04 20:06:38 -0300

    main page

---
+++ index.js
@@ -0,0 +1,24 @@
+const projects = [
+    {
+        name: 'four-card-feature-section-master'
+    }
+];
+
+const list = document.querySelector('.list');
+
+projects.forEach(({name}) => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+        <a href = './${name}/index.html'>
+            <img src = './${name}/design/desktop-preview.jpg' alt = '${name}'>
+            <p> ${projectName(name)}  </p>
+        </a>
+    `
+
+    list.appendChild(listItem);
+});
+
+
+function projectName(name) {
+    return name.split(' ').map(word => word[0].toUpperCase() + word.slice(1))
+}
